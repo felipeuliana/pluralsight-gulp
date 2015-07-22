a026291671ef0acbe085b6c6dd062153c6bea8e7
Felipe Uliana <v.uliana@gmail.com>
2015-07-22 14:38:09 -0300

    Finished Module: 'HTML Injection'

--- Gulpfile.js
+++ Gulpfile.js
@@ -37,6 +37,27 @@ gulp.task('less-watcher', function () {
   gulp.watch([config.less], ['styles']);
 });
 
+gulp.task('wiredep', function () {
+  log('Wiring the bower dependencies into the html');
+  var options = config.getWiredepDefaultOptions();
+  var wiredep = require('wiredep').stream;
+
+  return gulp
+    .src(config.index)
+    .pipe(wiredep(options))
+    .pipe($.inject(gulp.src(config.js)))
+    .pipe(gulp.dest(config.client));
+});
+
+gulp.task('inject', ['wiredep', 'styles'], function () {
+  log('Wiring the app css into the html, and call wiredep');
+
+  return gulp
+    .src(config.index)
+    .pipe($.inject(gulp.src(config.css)))
+    .pipe(gulp.dest(config.client));
+});
+
 //
 
 function clean(path, done) {
--- gulp.config.js
+++ gulp.config.js
@@ -1,8 +1,10 @@
 module.exports = function () {
   var client = './src/client/';
+  var clientApp = client + 'app/';
+  var temp = './.tmp/';
 
   var config = {
-    temp: './.tmp/',
+    temp: temp,
 
     /**
      * Files paths
@@ -12,8 +14,33 @@ module.exports = function () {
       './src/**/*.js',
       './*.js'
     ],
+    client: client,
+    css: temp + 'styles.css',
+    index: client + 'index.html',
+    js: [
+      clientApp + '**/*.module.js',
+      clientApp + '**/*.js',
+      '!' + clientApp + '**/*.spec.js',
+    ],
 
-    less: client + 'styles/styles.less'
+    less: client + 'styles/styles.less',
+
+    bower: {
+      json: require('./bower.json'),
+      directory: './bower_components/',
+      ignorePath: '../..'
+    }
+
+  };
+
+  config.getWiredepDefaultOptions = function () {
+    var options = {
+      bowerJson: config.bower.json,
+      directory: config.bower.directory,
+      ignorePath: config.bower.ignorePath
+    };
+
+    return options;
   };
 
   return config;
